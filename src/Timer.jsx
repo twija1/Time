3993e04b844cca9d15a8fd51e0dbbadb5feeabde
twija <kassdarmofal@gmail.com>
2020-03-24 17:32:43 +0100

    Added time save

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,10 +1,22 @@
 import React, {useEffect, useState} from "react";
 import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 function TimerWraper() {
     const [time0, setTime0] = useState(Date.now());
     const [isOn, setRunning] = useState(false);
     const [elapsedTime, setElapsedTime] = useState(0);
+    const [listOfTimes, setListOfTimes] = useState([]);
+
+    const addTime = () => {
+        const timeElapsed = new Date(elapsedTime);
+        const minutes = timeElapsed.getMinutes().toString().padStart(2, '0');
+        const seconds = timeElapsed.getSeconds().toString().padStart(2, '0');
+        const milliseconds = timeElapsed.getMilliseconds().toString().slice(0, -1).padStart(2, '0');
+        setListOfTimes([...listOfTimes, `${minutes}:${seconds}:${milliseconds}`])
+    };
 
     const resetTimer = () => {
         setTime0(Date.now());
@@ -26,29 +38,46 @@ function TimerWraper() {
         }
     };
 
+    const refreshTimeElapsed = () => {
+        setElapsedTime(Date.now() - time0);
+    };
+
+    useEffect(() => {
+        if (isOn) {
+            const idInterval = setInterval(refreshTimeElapsed, 10);
+            return () => clearInterval(idInterval)
+        }
+    });
+
+    const showListofTimes = listOfTimes.map(time => <ListItem><ListItemText>{time}</ListItemText></ListItem>);
+
     return (
         <div>
             <Timer time0={time0} isOn={isOn}/>
             <Button onClick={resetTimer}>reset</Button>
-            <Button onClick={startTimer}>start</Button>
-            <Button onClick={stopTimer}>stop</Button>
+            {!isOn?<Button onClick={startTimer}>start</Button>:<Button onClick={stopTimer}>stop</Button>}
+            <Button onClick={addTime}>add time</Button>
+            <List>
+                {showListofTimes}
+            </List>
         </div>
     )
 }
 
 function Timer({time0, isOn}) {
-    const [timer, setTimer] = useState('00:00');
+    const [timer, setTimer] = useState('00:00:00');
 
     const refreshTimer = () => {
         const timeElapsed = new Date(Date.now() - time0);
         const minutes = timeElapsed.getMinutes().toString().padStart(2, '0');
         const seconds = timeElapsed.getSeconds().toString().padStart(2, '0');
-        setTimer(`${minutes}:${seconds}`)
+        const milliseconds = timeElapsed.getMilliseconds().toString().slice(0, -1).padStart(2, '0');
+        setTimer(`${minutes}:${seconds}:${milliseconds}`)
     };
 
     useEffect(() => {
         if (isOn) {
-            const idInterval = setInterval(refreshTimer, 1000);
+            const idInterval = setInterval(refreshTimer, 10);
             return () => clearInterval(idInterval)
         }
     });
