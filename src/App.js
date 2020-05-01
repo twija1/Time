4286fa5627cc7658de2b0367effb2592f262fd26
twija <kassdarmofal@gmail.com>
2020-05-01 15:37:08 +0200

    Added navigation and switch

--- src/App.js
+++ src/App.js
@@ -2,15 +2,42 @@ import React from 'react';
 import './App.css';
 import Clock from "./Clock";
 import TimerWrapper from "./TimerWrapper"
+import SignIn from "./SignIn";
+import Container from "@material-ui/core/Container";
+import {Switch, Link} from 'react-router-dom'
+import Button from "@material-ui/core/Button";
+import PublicRoute from "./PublicRoute";
+import PrivateRoute from "./PrivateRoute";
+import * as lg from './helpers'
+import { Redirect } from 'react-router-dom'
 
 function App() {
 
-  return (
-    <div className="App">
-      <Clock/>
-      <TimerWrapper/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>
+                <Button component={Link} to='/clock'>clock</Button>
+                <Button component={Link} to='/stopper'>stopper</Button>
+                <Button component={Link} to='/'>signin</Button>
+                {lg.isLogin() ?
+                    <Button onClick={() => {
+                        lg.logout();
+                        window.location.reload()
+                    }}>logout</Button>
+                    : <Button onClick={() => {
+                        lg.login();
+                        window.location.reload()
+                    }}>login</Button>}
+            </div>
+            <Container>
+                <Switch>
+                    <PrivateRoute path='/clock' component={Clock}/>
+                    <PrivateRoute path='/stopper' component={TimerWrapper}/>
+                    <PublicRoute restricted={true} exact path='/' component={SignIn}/>
+                </Switch>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
