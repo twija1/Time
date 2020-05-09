a5cfb07e8f9e1c2b28f6a216caa4e04a273fc129
twija <kassdarmofal@gmail.com>
2020-05-09 22:09:43 +0200

    Added AppBar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,70 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import './App.css';
-import Clock from "./Clock";
-import TimerWrapper from "./TimerWrapper"
-import SignIn from "./SignIn";
+import Clock from "./Components/Clock";
+import TimerWrapper from "./Components/TimerWrapper"
+import SignIn from "./Components/SignIn";
 import Container from "@material-ui/core/Container";
 import {Switch, Link} from 'react-router-dom'
 import Button from "@material-ui/core/Button";
-import PublicRoute from "./PublicRoute";
-import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./Components/PublicRoute";
+import PrivateRoute from "./Components/PrivateRoute";
 import * as lg from './helpers'
-import LandingPage from "./LandingPage";
-import Register from "./Register";
+import LandingPage from "./Components/LandingPage";
+import Register from "./Components/Register";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import {makeStyles} from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3)
+    }
+}));
 
 function App() {
+    const classes = useStyles();
 
     return (
         <div className="App">
-            <div>
-                <Button component={Link} to='/clock'>clock</Button>
-                <Button component={Link} to='/stopper'>stopper</Button>
-                <Button component={Link} to='/signin'>signin</Button>
-                {lg.isLogin() ?
-                    <Button onClick={() => {
-                        lg.logout();
-                    }}>logout</Button>
-                    : undefined}
-                {lg.isLogin() ?
-                    undefined :
-                    <Button component={Link} to='/register'>register</Button>}
+            <div className={classes.root}>
+                <AppBar position='static'>
+                    <Toolbar>
+                        <IconButton edge='start' className={classes.menuButton} color='inherit' aria-label='menu'>
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant='h6' className={classes.title}>
+                            Langing Page
+                        </Typography>
+                        <Button color="inherit" component={Link} to='/clock'>clock</Button>
+                        <Button color="inherit" component={Link} to='/stopper'>stopper</Button>
+                        {lg.isLogin() ?
+                            undefined :
+                            <Button color="inherit" component={Link} to='/signin'>signin</Button>}
+                        {lg.isLogin() ?
+                            <Button color="inherit" onClick={() => {
+                                lg.logout();
+                            }}>logout</Button>
+                            : undefined}
+                        {lg.isLogin() ?
+                            undefined :
+                            <Button color="inherit" component={Link} to='/register'>register</Button>}
+                    </Toolbar>
+                </AppBar>
             </div>
-            <Container>
+            <Container className={classes.content}>
                 <Switch>
                     <PrivateRoute path='/clock' component={Clock}/>
                     <PrivateRoute path='/stopper' component={TimerWrapper}/>
